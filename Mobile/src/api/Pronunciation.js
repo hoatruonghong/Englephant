cffff9b23767c9eadbb7807f79e11d0fa060a9c2
TranLeDinh <tran.dinhle@hcmut.edu.vn>
2023-12-29 18:07:56 +0700

    Update APIs

--- Mobile/src/api/Pronunciation.js
+++ Mobile/src/api/Pronunciation.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const unlockPLessonDefault = async (params) => {
     try {
         console.log('https://englephant.vercel.app/api/pronunciation/all-lessons/'+params.learnerId);
-        const res = await axios.post('https://englephant.vercel.app/pronunciation/all-lessons/'+params.learnerId);
+        const res = await axios.post('https://englephant.vercel.app/api/pronunciation/all-lessons/'+params.learnerId);
         console.log(res.data);
         return true
     } catch (error) {
